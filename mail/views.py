6cef41f6236e727ea7f00fe48bbec9972af51060
KJ Tech <Janakiramankeerthivelan21@gmail.com>
2023-04-25 17:52:20 +0530

    Added mail recieved reading function

--- mail/views.py
+++ mail/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.core.mail import send_mail,get_connection,EmailMessage
 from .forms import EmailForm
 from .templates import *
+import imaplib
+import email
+from django.shortcuts import render
+
 
 def send_email(request):
     if request.method == 'POST' or request.method == 'GET':
@@ -27,13 +31,30 @@ def send_email(request):
 
     return render(request,'sender_email.html',{'form':form})
 
-def readmail(request):
-    if request.method == 'POST' or request.method == 'GET':
-        connection = get_connection(host='mail.google.com', port = 587 , username = 'janakiramankeerthivelan21@gmail.com' , password = 'alishksxhsjjdcnv')
-        connection.open()
 
-        inbox = connection.folder('INBOX')
-        message = inbox[-1]
 
-        print("subject : " , message.subject)
-        print("Body : " , message.body)
+
+
+def read_email(request):
+    # Connect to the Gmail IMAP server and login
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login('janakiramankeerthivelan21@gmail.com', 'alishksxhsjjdcnv')
+
+    # Select the inbox folder and retrieve all emails
+    mail.select('inbox')
+    result, data = mail.uid('search', None, "ALL")
+    email_uids = data[0].split()
+
+    # Iterate through all emails and extract relevant information
+    emails = []
+    for uid in email_uids:
+        result, email_data = mail.uid('fetch', uid, '(RFC822)')
+        raw_email = email_data[0][1].decode('utf-8')
+        email_message = email.message_from_string(raw_email)
+        sender = email.utils.parseaddr(email_message['From'])[1]
+        subject = email_message['Subject']
+        body = email_message.get_payload()
+        emails.append({'sender': sender, 'subject': subject, 'body': body})
+
+    # Render the email details in a template
+    return render(request, 'read_email.html', {'emails': emails})
--- mailsys/urls.py
+++ mailsys/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from mail.views import send_email,readmail
+from mail.views import send_email,read_email
 
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('send_email/',send_email,name='send_email'),
-    path('read/',readmail,name='readmail'),
+    path('read/',read_email,name='read_email'),
 ]
